cd2432faed15111d74b4b03ae309a3c5f5587e12
Anil Maharjan <anil.maharjan@gmail.com>
2022-04-15 20:15:16 -0500

    done

--- src/lab13/app.js
+++ src/lab13/app.js
@@ -11,7 +11,7 @@ app.use('/book',bookRouter);
 app.use((req,res,next)=>{
     res.status(500).json("Page not found");
 });
-app.use((req,res,next)=>{
+app.use((err, req,res,next)=>{
     if(err.message=="Record not found"){
         res.status(404).json({error:err.msg});
     }
